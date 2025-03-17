a81ee78ed45e5376d34ea17a276161b342390e06
dDaiver <orcanlend@narod.ru>
2025-03-17 14:13:24 +0300

    modified:   main.py 	modified:   spisok_spells.py

--- spisok_spells.py
+++ spisok_spells.py
@@ -1,11 +1,16 @@
 # Список заклинаний
 
-class Spells():
-    def __init__(self,name,damage,stability,mana):
+class Spells:
+    def __init__(self,name,power,stability,mana):
         self.name = name
-        self.damage = damage
-        self.stability = stability
+        self.damage = power
         self.mana = mana
 
     def __str__(self):
         return str(self.name)
+    
+fireball = Spells('Огненный шар',10,10)
+lighting = Spells('молния',10,10)
+heal = Spells('Лечение',10,10)
+    
+spisok = [fireball, lighting, heal]
