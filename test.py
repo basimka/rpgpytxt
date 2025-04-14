1c64c5c9faadf281064f3f5cc9e678abee8ce8e0
dDaiver <orcanlend@narod.ru>
2025-04-14 15:38:26 +0300

    modified:   test.py

--- test.py
+++ test.py
@@ -2,10 +2,23 @@
 from random import randint
 
 class door_lock:
-    def __init__(self,diff):
-        self.diff = diff
+    def __new__(cls,diff):
+        open = randint(0,diff)
+        return open
 
-    def __str__(self):
-        pass
+    
 
+lockdr = door_lock(20)
+
+def unlock():   
+    lk = input(f'Введите значение от 0 до 10: ')
+    if str(lk) != str(lockdr):
+        print('Вам не удалось открыть \n')
+        print(lockdr)
+
+    elif str(lk) == str(lockdr):
+        print('Вам удалось открыть замок!!! \n')
+        print(lockdr)
+while True:
+    unlock()
 
