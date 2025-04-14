b896a6ac431c6e0ff62f474f4464e8b7deb65f02
dDaiver <orcanlend@narod.ru>
2025-04-14 13:56:20 +0300

    modified:   test.py

--- test.py
+++ test.py
@@ -1,34 +1,11 @@
-class Weapon:
-    def __init__(self,name,damage,stability):
-        self.name = name
-        self.damage = damage
-        self.stability = stability
+# Для начала импортируем рандомайзер
+from random import randint
+
+class door_lock:
+    def __init__(self,diff):
+        self.diff = diff
 
     def __str__(self):
-        return str(self.name)
+        pass
 
-woodstick = Weapon('Палка',10,5)
-clows = Weapon('Когти',20,20)
-knife = Weapon('Нож',20,10)
-sword = Weapon('Меч',25,15)
-staff = Weapon('Посох',15,15)
 
-class inventar():
-    golds = 0
-    things = [sword,staff,sword]
-    str_things = []
-    potions = []
-    scrolls = []
-    limit = 10
-
-inv = inventar()
-
-i =0
-
-while i < len(inv.things):
-    inv.str_things.append(str(inv.things[i]))
-    i+=1
-
-print (inv.things[0])
-print (sword)
-print (inv.str_things)
