9097332d0a866c3cd66a340f1714a124a960834c
dDaiver <orcanlend@narod.ru>
2025-05-03 11:00:56 +0300

    modified:   old/test3.py

--- old/test3.py
+++ old/test3.py
@@ -3,25 +3,26 @@ from random import randint
 class Lock:
     def __init__(self,diff):
         self.diff = diff
-
+    
     @staticmethod
     def difficult():
-        d1 = randint(0,10)
+        d1 = randint(0, 10)
         return d1
+    
+    dd = difficult()
 
-    def unlock(self):
+    def unlock(self,d1):
         vzlom = input ("ВВедите число от 0 до 10 ")
-        if vzlom == self.difficult():
+        if int(vzlom) == int(d1):
             print ("Вы открыли замок!!!")
         else:
             print ("ЗАКРЫТО!!!")
-            print (self.difficult())
+            print (d1)
 
     def __str__(self):
         pass
     
-
 zamok = Lock(10)
 
 while True:
-    zamok.unlock()
+    zamok.unlock(zamok.dd)
